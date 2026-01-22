ab629f3cbf40155876ce7a090c7dfdbc7c0a562f
sigrid.edberg <sigrid.edberg@gmail.com>
2026-01-22 12:19:18 +0000

    change landing page

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/')
-def hello():
-    return 'Hello, Jonas!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
