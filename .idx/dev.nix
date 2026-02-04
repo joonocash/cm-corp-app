b932721313396eae43c95931b2a0974fc5af2f7f
89jonand <89jonand@gafe.molndal.se>
2026-02-04 09:44:00 +0000

    Add Docker support for CI/CD pipeline

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,6 +10,9 @@
      pkgs.python311Packages.pip
      pkgs.git
      pkgs.github-cli
+     pkgs.sqlite
+     pkgs.azure-cli
+     pkgs.openssl
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
